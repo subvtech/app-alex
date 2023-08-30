efd0513ce6c57fc43e32def53f7fdd141a088868
maikaodev <maikao.dev@gmail.com>
2023-08-30 12:49:06 -0300

    feat: add and config nuxt-vitest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
-  modules: ['@pinia/nuxt', '@nuxtjs/strapi'],
+  modules: ['@pinia/nuxt', '@nuxtjs/strapi', 'nuxt-vitest'],
   pinia: {
     autoImports: ['defineStore', 'storeToRefs'],
   },
---
+++ test/pages/login.nuxt.test.ts
@@ -0,0 +1,15 @@
+// @vitest-environment nuxt
+
+import { describe, it, expect } from 'vitest';
+import { screen } from '@testing-library/vue';
+
+import { renderSuspended } from 'nuxt-vitest/utils';
+import Login from '../../pages/login/index.vue';
+
+describe('Login', () => {
+  it(`should show 'Bem vindo ao projeto ALEX!`, async () => {
+    await renderSuspended(Login);
+
+    expect(screen.getAllByText('Bem vindo a plataforma ALEX!')).toBeDefined();
+  });
+});
