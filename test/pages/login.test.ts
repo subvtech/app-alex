42066bb6a1a8fc136130caa0c414f9ba7ffcc2fa
Angelo Danrley <maikao.dev@gmail.com>
2023-09-06 14:28:56 -0300

    test: created for userFormRules schemas 1 and 2

--- test/pages/login.test.ts
+++ test/pages/login.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { screen } from '@testing-library/vue';
 
 import { renderSuspended } from 'nuxt-vitest/utils';
-import Login from '~/login/index.vue';
+import Login from '@/pages/login/index.vue';
 
 describe('Login', () => {
   it(`should show 'Bem vindo ao projeto ALEX!`, async () => {
