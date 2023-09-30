ebd4576cd2c78734065158b3b76f29795ac3ba6d
zignaggo <ziggnago@gmail.com>
2023-09-30 15:16:20 -0300

    fix: threads toggle button

--- components/alex/inputs/BtnToggle.vue
+++ components/alex/inputs/BtnToggle.vue
@@ -1,17 +1,16 @@
 <template>
-    <v-btn-secondary
-      v-if="!hasCustomBtn"
-      :icon="active.icon"
-      :value="active.value"
-      @click="onClick"
-    />
-    <slot
-      v-else
-      :icon="active.icon"
-      :value="active.value"
-      :click="onClick"
-    >
-    </slot>
+  <v-btn-secondary
+    v-if="!hasCustomBtn"
+    :icon="activeItem.icon"
+    :value="activeItem.value"
+    @click="onClick"
+  />
+  <slot
+    v-else
+    :icon="activeItem.icon"
+    :value="activeItem.value"
+    :click="onClick"
+  />
 </template>
 
 <script setup lang="ts">
@@ -25,16 +24,18 @@ const props = defineProps<{
 }>();
 const emit = defineEmits(['update:modelValue']);
 const slots = useSlots();
-const active = computed(() => props.items.filter((item) => item.value == props.modelValue)[0])
-
+const activeItem = computed(
+  () => props.items.filter((item) => item.value == props.modelValue)[0],
+);
 const hasCustomBtn = computed(() => !!slots.default);
+const activeItemIndex = computed(() =>
+  props.items.findIndex((item) => item.value === props.modelValue),
+);
 const onClick = () => {
-  const currentIndex =  props.items.findIndex((item) => item.value === props.modelValue);
+  const nextItemIndex = activeItemIndex.value + 1;
+  const maxItemIndex = props.items.length - 1;
   const nextItem =
-    currentIndex + 1 > props.items.length - 1 ? props.items[0] : props.items[currentIndex + 1];
+    nextItemIndex > maxItemIndex ? props.items[0] : props.items[nextItemIndex];
   emit('update:modelValue', nextItem.value);
 };
-
 </script>
-
-<style scoped></style>
