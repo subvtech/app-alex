66b08d4cc088cbf2f384c3627423b75185f66430
chrixtianny <christianny@live.com>
2023-10-09 21:26:38 -0300

    tutorial em componente baseado no do zig

---
+++ components/alex/tutorial/stepper/index.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <!-- Renderização das etapas do tutorial -->
+    <div v-if="showSteps">
+      <div v-if="!noHeader" class="d-flex gap-4 py-3 px-1">
+        <alex-tutorial-stepper-item
+          v-for="item in tutorialStepsData"
+          :key="item.step"
+          :title="item.title"
+          :subtitle="item.subtitle"
+          :stepNumber="item.step"
+          :completed="currentStep > item.step"
+          class="custom-style"
+          @onSelect="() => currentStep = item.step"
+        />
+      </div>
+
+      <!-- Conteúdo da etapa atual -->
+      <alex-tutorial-stepper-window>
+        <alex-tutorial-stepper-window-item>
+          <!-- Título e conteúdo da etapa atual -->
+          <v-card flat class="tutorial-content py-10">
+            <v-card-title>{{
+              tutorialStepsData[currentStep - 1].title
+            }}</v-card-title>
+            <div class="mt-5 ml-10 mb-15 px-5 text-justify" v-html="tutorialStepsData[currentStep-1].content"></div>
+            <alex-tutorial-stepper-actions
+              v-if="!showControls"
+              class="d-flex py-10 px-5">
+              <v-btn
+                v-if="currentStep > 1 && showNextButton"
+                type="button"
+                rounded="lg"
+                color="accent"
+                variant="outlined"
+                size="large"
+                @click="onPrevStep"
+                text="Voltar"
+              />
+
+              <v-btn
+                v-if="showNextButton"
+                @click="nextStep"
+                class="ml-auto"
+                rounded="lg"
+                color="accent"
+                size="large"
+                text="Avançar"
+              />
+               <v-btn
+                v-if="!showNextButton"
+                class="next-step"
+                @click="restartTutorial"
+                rounded="lg"
+                color="accent"
+                size="large"
+                text="Reiniciar"
+               />
+                <v-btn
+                  v-if="!showNextButton"
+                  @click="finishTutorial"
+                  class="ml-auto"
+                  rounded="lg"
+                  color="accent"
+                  size="large"
+                  text="Finalizar"
+                />
+              <slot
+                name="controls"
+                :onPrevStep="onPrevStep"
+                :isLastStep="currentStep == tutorialStepsData.length"
+              />
+            </alex-tutorial-stepper-actions>
+          </v-card>
+        </alex-tutorial-stepper-window-item>
+      </alex-tutorial-stepper-window>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, defineProps } from 'vue';
+
+// Prop para passar os dados das etapas do tutorial
+const { tutorialStepsData } = defineProps(['tutorialStepsData']);
+
+const currentStep = ref(1);
+
+const showSteps = computed(
+  () => currentStep.value <= tutorialStepsData.length,
+);
+const showNextButton = computed(
+  () => currentStep.value < tutorialStepsData.length,
+);
+
+const nextStep = () => {
+  if (currentStep.value < tutorialStepsData.length) {
+    currentStep.value++;
+  }
+};
+
+const onPrevStep = () => {
+  if (currentStep.value > 1) {
+    currentStep.value--;
+  }
+};
+
+const restartTutorial = () => {
+  currentStep.value = 1;
+};
+
+const finishTutorial = () => {
+  if (currentStep.value == tutorialStepsData.length) {
+    window.location.href = '/';
+  }
+};
+</script>
---
+++ components/alex/tutorial/stepper/item.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-hover #default="{ isHovering, props }">
+    <div
+      v-bind="props"
+      class="d-flex rounded-lg align-center justify-center gap-4 w-fit pa-2 cursor-pointer active"
+      :class="useClasses(active, disabled, isHovering).container"
+      @click="emit('onSelect')"
+    >
+      <div
+        class="step-indicator-number-icon rounded-lg"
+        :class="useClasses(active, disabled, isHovering).bgNumber"
+    >
+        <v-icon
+          v-if="icon || completed"
+          :icon="completed ? 'mdi-check' : icon"
+          :class="useClasses(active, disabled, isHovering).number"
+        />
+        <div
+          v-else
+          class="text-h4"
+          :class="useClasses(active, disabled, isHovering).number"
+        >
+          {{ stepNumber }}
+        </div>
+      </div>
+      <div class="d-flex flex-column align-start justify-center text-gray-300">
+        <div
+          class="text-body-2"
+          :class="useClasses(active, disabled, isHovering).title"
+        >
+          {{ title }}
+        </div>
+        <div
+          v-if="subtitle"
+          class="text-body-3"
+          :class="useClasses(active, disabled, isHovering).subtitle"
+        >
+          {{ subtitle }}
+        </div>
+      </div>
+    </div>
+  </v-hover>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['onSelect']);
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: { type: String },
+  icon: { type: String },
+  stepNumber: { type: Number, required: true },
+  checked: { type: Boolean, default: false },
+  active: { type: Boolean, default: false },
+  disabled: { type: Boolean, default: true },
+  completed: { type: Boolean, default: false },
+});
+
+const states = {
+  hovering: {
+    title: 'text-gray-600',
+    subtitle: 'text-gray-400',
+    bgNumber: 'bg-gray-blue',
+    number: 'text-gray-400 number',
+    container: 'border-1-gray-blue elevation-4',
+  },
+  active: {
+    title: 'text-gray-600',
+    subtitle: 'text-gray-400',
+    bgNumber: 'bg-secondary-0',
+    number: 'text-white',
+    container: '',
+  },
+  default: {
+    title: 'text-gray-300',
+    subtitle: 'text-gray-300',
+    bgNumber: 'bg-gray-blue',
+    number: 'text-gray-300 ',
+    container: '',
+  },
+  click: {
+    title: 'text-gray-600',
+    subtitle: 'text-gray-400',
+    bgNumber: 'bg-gray-blue',
+    number: 'text-secondary-0',
+    container: '',
+  },
+};
+const useClasses = (
+  active: boolean,
+  disabled: boolean,
+  hovering?: boolean,
+  click?: boolean,
+) => {
+  if (click) return states['click'];
+  else if (hovering && !active && !disabled) return states['hovering'];
+  else if (active) return states['active'];
+  else return states['default'];
+};
+</script>
+
+<style scoped lang="scss">
+/* Personalize as classes de estilo conforme necessário */
+.step-indicator-number-icon {
+  display: grid;
+  place-items: center;
+  width: 40px;
+  height: 40px;
+}
+.border-1-gray-blue {
+  outline: 1px solid rgb(var(--v-theme-gray-100));
+}
+.w-fit {
+  width: fit-content;
+  height: fit-content;
+}
+.cursor-pointer {
+  cursor: pointer;
+}
+.active:active .number {
+  color: rgb(var(--v-theme-secondary-0)) !important;
+}
+/* Adicione suas personalizações de estilo aqui */
+</style>
