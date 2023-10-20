bcc9dcd8ed0fd72231dfc0745a014d2993c27d31
chrixtianny <christianny@live.com>
2023-10-20 16:05:02 -0300

    primeira versão do checkbox

---
+++ components/TailoredCheckbox.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container" width="113" max-width="fit-content" height="47">
+    <div class="checkbox-container mr-1 pa-0 mt-0 pink-500">
+      <v-checkbox
+        :class="{ 'input-checkbox': checkbox.indeterminate }"
+        :model-value="checkbox.modelValue"
+        :indeterminate="checkbox.indeterminate"
+        :disabled="checkbox.disabled"
+        :readonly="checkbox.readonly"
+        hide-details="auto"
+        color="#00B7CC"
+        width="18"
+      />
+    </div>
+    <div
+      class="text-container"
+      :class="{ 'no-hint': !checkbox.hint }"
+      width="auto"
+    >
+      <span
+        class="title"
+        :class="{ 'gray-400': checkbox.disabled || checkbox.readonly }"
+        >{{ checkbox.label }}</span
+      >
+      <span
+        class="hint"
+        :class="{ 'gray-400': checkbox.disabled || checkbox.readonly }"
+      >
+        {{ checkbox.hint }}
+      </span>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  checkbox: {
+    type: Object,
+    required: true,
+  },
+});
+
+const checkbox = props.checkbox;
+</script>
+<style scoped>
+div {
+  font-family: Sen !important;
+  display: flex;
+  align-items: flex-start;
+}
+
+.container {
+  display: flex;
+  align-items: flex-end;
+  width: fit-content;
+  height: 47px;
+}
+
+.text-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  max-width: 100%;
+}
+
+.title {
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 21.6px;
+  color: #454d54;
+  display: flex;
+  align-items: flex-start;
+}
+
+.gray-400 {
+  color: #a0a8b1 !important;
+}
+
+.hint {
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18.9px;
+  color: #6e7a87;
+}
+
+.no-hint {
+  margin-bottom: 1.2rem !important;
+}
+
+.input-checkbox {
+  color: #00b7cc !important;
+}
+</style>
--- pages/Checkbox.vue
+++ pages/Checkbox.vue
@@ -1,101 +1,94 @@
 <template>
-  <v-container fluid>
-    <span>
-      <div class="container" width="100%" height="47">
-        <div class="inner-container">
-          <v-checkbox id="checkboxes" :model-value="true" color="#00B7CC" />
-        </div>
-        <div class="inner-text">
-          <span class="title">{{ label }}</span>
-          <span class="hint">{{ hint }}</span>
-        </div>
-      </div>
-    </span>
-    <span>
-      <div class="container" width="100%" height="47">
-        <div class="inner-container">
-          <v-checkbox :model-value="false" />
-        </div>
-        <div class="inner-text">
-          <span>{{ label }}</span>
-          <span class="hint">{{ hint }}</span>
-        </div>
-      </div>
-    </span>
-    <span>
-      <div class="container" width="100%" height="47">
-        <div class="inner-container">
-          <v-checkbox indeterminate color="#00B7CC" value="#00B7CC" />
-        </div>
-        <div class="inner-text">
-          <span>{{ label }}</span>
-          <span class="hint">{{ hint }}</span>
-        </div>
-      </div>
-    </span>
-    <span>
-      <div class="container" width="100%" height="47">
-        <div class="inner-container">
-          <v-checkbox :model-value="true" disabled />
-        </div>
-        <div class="inner-text">
-          <span>{{ label }}</span>
-          <span class="hint">{{ hint }}</span>
-        </div>
-      </div>
-    </span>
-    <span>
-      <div class="container" width="100%" height="47">
-        <div class="inner-container">
-          <v-checkbox :model-value="false" disabled />
-        </div>
-        <div class="inner-text">
-          <span>{{ label }}</span>
-          <span class="hint">{{ hint }}</span>
-        </div>
-      </div>
-    </span>
-  </v-container>
+  <TailoredCheckbox :checkbox="checkbox1" />
+  <TailoredCheckbox :checkbox="checkbox2" />
+  <TailoredCheckbox :checkbox="checkbox3" />
+  <TailoredCheckbox :checkbox="checkbox4" />
+  <TailoredCheckbox :checkbox="checkbox5" />
+  <TailoredCheckbox :checkbox="checkbox6" />
+  <TailoredCheckbox :checkbox="checkbox7" />
+  <TailoredCheckbox :checkbox="checkbox8" />
+  <TailoredCheckbox :checkbox="checkbox9" />
 </template>
 <script setup>
 import { ref } from 'vue';
 
-const label = ref('Text value');
-const hint = ref('Hint');
+const checkbox1 = ref({
+  modelValue: true,
+  label: 'Checkbox',
+  hint: 'True',
+  disabled: false,
+  readonly: false,
+  indeterminate: false,
+});
+
+const checkbox2 = ref({
+  modelValue: false,
+  label: 'Teste sem hint',
+  hint: '',
+  disabled: false,
+  readonly: false,
+  indeterminate: false,
+});
+
+const checkbox3 = ref({
+  modelValue: false,
+  label: 'Checkbox',
+  hint: 'Indeterminate and true',
+  disabled: false,
+  readonly: false,
+  indeterminate: true,
+});
+
+const checkbox4 = ref({
+  modelValue: false,
+  label: 'Checkbox',
+  hint: 'Disabled and false',
+  disabled: true,
+  readonly: false,
+  indeterminate: false,
+});
+
+const checkbox5 = ref({
+  modelValue: true,
+  label: 'Checkbox',
+  hint: 'Disabled and true',
+  disabled: true,
+  readonly: false,
+  indeterminate: false,
+});
+
+const checkbox6 = ref({
+  modelValue: true,
+  label: 'Checkbox',
+  hint: 'Disabled and indeterminate',
+  disabled: true,
+  readonly: false,
+  indeterminate: true,
+});
+
+const checkbox7 = ref({
+  label: 'Checkbox',
+  hint: 'Indeterminate and readonly',
+  disabled: false,
+  readonly: true,
+  indeterminate: true,
+});
+
+const checkbox8 = ref({
+  modelValue: true,
+  label: 'Checkbox',
+  hint: 'Readonly and true',
+  disabled: false,
+  readonly: true,
+  indeterminate: false,
+});
+
+const checkbox9 = ref({
+  modelValue: false,
+  label: 'Checkbox',
+  hint: 'Readonly and false',
+  disabled: false,
+  readonly: true,
+  indeterminate: false,
+});
 </script>
-<style scoped>
-v-container {
-  font-family: Sen !important;
-}
-
-.container {
-  display: flex;
-  align-items: center;
-  margin-top: 16px;
-  width: 113px;
-  height: 47px;
-  justify-content: flex-start;
-}
-
-.title {
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 21.6px;
-  letter-spacing: 2%;
-}
-
-.inner-text {
-  display: flex;
-  flex-direction: column;
-  min-width: 81px;
-  margin-left: 16px;
-}
-
-.hint {
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 18.9px;
-  letter-spacing: 2%;
-  color: #6e7a87;
-}
-</style>
