61c8130593ea027ccbb87866fcfde5da2713ce1b
chrixtianny <christianny@live.com>
2023-10-26 00:29:18 -0300

    criação do componente Dropdown e uso em page

---
+++ components/Dropdown.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="text-center">
+    <v-menu
+      :open-on-click="props.options.openOnClick"
+      :open-on-hover="props.options.openOnHover"
+      :close-on-content-click="props.options.closeOnContentClick"
+      :location="props.options.location"
+      :dark="props.options.isDarkTheme"
+      min-width="200"
+      border-radius="8"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          style="
+            background-color: white !important;
+            box-shadow: none;
+            color: #4f4f4f !important;
+          "
+          icon="mdi-dots-vertical"
+          v-bind="props"
+        ></v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="item in props.items"
+          :key="item.id"
+          :class="itemClasses(item)"
+          @mouseover="item.isHover = true"
+          @mouseout="item.isHover = false"
+        >
+          <template v-if="item.type === 'text'">
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </template>
+          <template v-else-if="item.type === 'icon'">
+            <v-list-item-title>
+              <v-icon>{{ item.icon }}</v-icon> {{ item.text }}
+            </v-list-item-title>
+          </template>
+          <template v-else-if="item.type === 'checkbox'">
+            <v-list-item-action>
+              <alex-inputs-checkbox :checkbox="checkboxDropdown" />
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { Checkbox as CheckboxProps } from '@/components/alex/inputs/Checkbox.vue';
+
+const checkboxDropdown: CheckboxProps = {
+  modelValue: false,
+  indeterminate: false,
+  disabled: false,
+  readonly: false,
+};
+
+type Item = {
+  id: number;
+  type: string;
+  icon?: string;
+  text: string;
+};
+
+type Options = {
+  openOnClick: boolean;
+  openOnHover: boolean;
+  closeOnContentClick: boolean;
+  location: string;
+  isDarkTheme: boolean;
+};
+
+const props = defineProps({
+  items: Array as () => Item[],
+  options: Object as () => Options,
+});
+
+const itemClasses = (item) => ({
+  default: !item.isHover && !item.isDisabled && !item.isWarning,
+  hover: item.isHover,
+  warning: item.isWarning,
+  disabled: item.isDisabled,
+  'dark-theme': props.options.isDarkTheme,
+});
+</script>
+
+<style scoped>
+div {
+  font-family: Sen !important;
+  font-weight: 700;
+  font-size: 14px;
+}
+.default {
+  color: #6e7a87;
+}
+
+.default.light-theme {
+  background-color: white;
+}
+
+.default.dark-theme {
+  background-color: #001a33;
+}
+
+.hover {
+  color: #30363b;
+  background-color: #ebedef;
+}
+
+.hover.dark-theme {
+  color: #fff;
+  background-color: #042749;
+}
+
+.warning {
+  color: #e9494a;
+}
+
+.warning.dark-theme {
+  color: #e9494a;
+  background-color: #001a33;
+}
+.warning.hover.dark-theme {
+  color: #e9494a;
+  background-color: #042749;
+}
+
+
+</style>
---
+++ pages/dropdown.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <Dropdown :items="items1" :options="options1" />
+    <Dropdown :items="items2" :options="options1" />
+    <Dropdown :items="items3" :options="options2" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Dropdown from '@/components/Dropdown.vue'; // Certifique-se de que o caminho do import está correto
+
+const items1 = ref([
+  {
+    id: 1,
+    type: 'icon',
+    icon: 'mdi-home',
+    text: 'Início',
+  },
+  {
+    id: 2,
+    type: 'icon',
+    icon: 'mdi-account-circle',
+    text: 'Perfil',
+  },
+  {
+    id: 3,
+    type: 'icon',
+    icon: 'mdi-account',
+    text: 'Configurações',
+  },
+]);
+
+const items2 = ref([
+  {
+    id: 1,
+    type: 'icon',
+    icon: 'mdi-lock',
+    text: 'Bloquear',
+  },
+  {
+    id: 2,
+    type: 'icon',
+    icon: 'mdi-pencil',
+    text: 'Editar',
+  },
+  {
+    id: 3,
+    type: 'icon',
+    icon: 'mdi-check-decagram-outline',
+    text: 'Verificar',
+  },
+  {
+    id: 4,
+    type: 'icon',
+    icon: 'mdi-delete',
+    text: 'Excluir',
+    isWarning: true,
+  },
+]);
+
+const items3 = ref([
+  {
+    id: 1,
+    type: 'checkbox',
+    text: 'Opção 1',
+  },
+  {
+    id: 2,
+    type: 'checkbox',
+    text: 'Opção 2',
+  },
+  {
+    id: 3,
+    type: 'checkbox',
+    text: 'Opção 3',
+  },
+]);
+
+const options1 = ref({
+  openOnClick: true,
+  openOnHover: true,
+  closeOnContentClick: true,
+  location: 'end',
+  isDarkTheme: false,
+});
+
+const options2 = ref({
+  openOnClick: true,
+  openOnHover: false,
+  closeOnContentClick: true,
+  location: 'start',
+  isDarkTheme: true,
+});
+</script>
