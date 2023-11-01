b759bdcdd7e1ba055252f98c80de1bebf7ce01f3
Eliezir <eliezir2004@gmail.com>
2023-11-01 03:24:14 -0300

    Add custom tabs component

---
+++ components/alex/custom/Tabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-tabs class="mb-10 text-gray-800" color="accent">
+    <v-tab
+      v-for="tab in tabs"
+      :key="tab.value"
+      :value="tab.value"
+      class="text-body-1 tab"
+      :class="!tab.notification ? 'px-7' : ''"
+      >{{ tab.label }}
+      <v-icon
+        v-if="tab.notification"
+        class="ml-1 mt-1"
+        icon="mdi-circle-medium"
+        color="accent"
+      />
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  tabs: {
+    type: Array as PropType<
+      { label: string; value: string; notification?: boolean }[]
+    >,
+    default: () => [{}],
+  },
+});
+</script>
+
+<style>
+.tab:not(.v-tab--selected):hover {
+  background-color: #ebedef !important;
+}
+</style>
