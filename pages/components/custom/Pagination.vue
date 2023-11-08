a400d740056cc992f791d3d70a1ac704d522a459
Eliezir <eliezir2004@gmail.com>
2023-11-07 22:05:46 -0300

    feature: component doc

---
+++ pages/components/custom/Pagination.vue
@@ -0,0 +1,243 @@
+<template>
+  <v-container
+    class="page rounded-lg bg-white pa-6 d-flex flex-column align-start pb-15"
+  >
+    <h1 class="text-h2 text-gray-800">Pagination</h1>
+    <p class="text-subtitle-2 text-gray-500">
+      O componente
+      <strong class="bg-accent pa-1 rounded">alex-custom-pagination</strong>
+      é usado para separar grande quantidade de dados em páginas, facilitando a
+      navegação do usuário.
+    </p>
+    <div class="d-flex align-center pa-4 container rounded-lg">
+      <img class="align-self-stretch" src="@/assets/svg/vuetify.svg" />
+      <div>
+        <p class="text-body-1 text-primary align-self-stretch">
+          Esse componente utiliza a estrutura do Vuetify, acesse ele a partir
+          desse link e veja mais informações.<br />
+        </p>
+        <a
+          class="text-decoration-underline text-blue"
+          href="https://vuetifyjs.com/en/components/paginations/"
+          target="_blank"
+          >vuetifyjs.com</a
+        >
+      </div>
+    </div>
+    <h2 class="text-h3 text-gray-800">Uso do Componente</h2>
+    <p class="text-subtitle-2 text-gray-500">
+      O componente deve receber obrigatoriamente a props
+      <strong>length</strong> indicando o número de páginas que o componente
+      deve ter e um <strong>v-model</strong> para o controle da pagina ativa.
+    </p>
+    <div class="pa-4 w-100">
+      <alex-custom-pagination :length="7" />
+    </div>
+    <div class="w-100">
+      <div
+        class="d-flex align-center justify-space-between w-100 px-3 bg-gray-100 rounded-t"
+      >
+        <alex-custom-tabs
+          v-model="firstExampleActivePage"
+          :tabs="firstExampleTabs"
+          color="black"
+        ></alex-custom-tabs>
+        <v-icon icon=" mdi-code-tags" color="gray-600" />
+      </div>
+      <v-window v-model="firstExampleActivePage" class="w-100">
+        <v-window-item value="1">
+          <div class="bg-grey-lighten-5 pa-3" style="position: relative">
+            <prism class="bg-grey-lighten-5">
+              {{ firstExampleTemplate }}
+            </prism>
+            <v-btn
+              class="copy-icon"
+              variant="text"
+              color="gray-400"
+              @click="copyToClipboard(firstExampleTemplate, 'firstTemplate')"
+            >
+              <v-icon
+                v-if="copiedValue == 'firstTemplate'"
+                size="x-large"
+                icon="mdi-clipboard-check-multiple-outline"
+                color="green-lighten-1"
+              />
+              <v-icon v-else size="x-large" icon="mdi-content-copy" />
+            </v-btn>
+          </div>
+        </v-window-item>
+        <v-window-item value="2">
+          <div class="bg-grey-lighten-5 pa-3" style="position: relative">
+            <prism class="bg-grey-lighten-5">
+              {{
+                ` 
+&lt;script setup&gt;
+${firstExampleScript}
+&lt;/script&gt;
+              `
+              }}
+            </prism>
+            <v-btn
+              class="copy-icon"
+              variant="text"
+              color="gray-400"
+              @click="copyToClipboard(firstExampleScript, 'firstScript')"
+            >
+              <v-icon
+                v-if="copiedValue == 'firstScript'"
+                size="x-large"
+                icon="mdi-clipboard-check-multiple-outline"
+                color="green-lighten-1"
+              />
+              <v-icon v-else size="x-large" icon="mdi-content-copy" />
+            </v-btn>
+          </div>
+        </v-window-item>
+      </v-window>
+    </div>
+    <h2 class="text-h3 text-gray-800">
+      Variante: Limitando o numero de paginas exibidas
+    </h2>
+    <p class="text-subtitle-2 text-gray-500">
+      Caso deseje, você pode limitar o número de páginas exibidas no componente,
+      para isso basta passar a props <strong>total-visible</strong> com o número
+      de páginas, caso não seja passado ou caso o número de paginas totais seja
+      menor que o número de páginas visíveis, o componente irá exibir todas as
+      páginas normalmente.
+    </p>
+    <div class="pa-4 d-flex w-100">
+      <div class="w-100">
+        <alex-custom-pagination :length="7" :total-visible="5" />
+      </div>
+    </div>
+    <div class="w-100">
+      <div
+        class="d-flex align-center justify-space-between w-100 px-3 bg-gray-100 rounded-t"
+      >
+        <alex-custom-tabs
+          v-model="secondExampleActivePage"
+          :tabs="firstExampleTabs"
+          color="black"
+        ></alex-custom-tabs>
+        <v-icon icon=" mdi-code-tags" color="gray-600" />
+      </div>
+      <v-window v-model="secondExampleActivePage" class="w-100">
+        <v-window-item value="1">
+          <div class="bg-grey-lighten-5 pa-3" style="position: relative">
+            <prism class="bg-grey-lighten-5">
+              {{ secondExampleTemplate }}
+            </prism>
+            <v-btn
+              class="copy-icon"
+              variant="text"
+              color="gray-400"
+              @click="copyToClipboard(secondExampleTemplate, 'secondTemplate')"
+            >
+              <v-icon
+                v-if="copiedValue == 'secondTemplate'"
+                size="x-large"
+                icon="mdi-clipboard-check-multiple-outline"
+                color="green-lighten-1"
+              />
+              <v-icon v-else size="x-large" icon="mdi-content-copy" />
+            </v-btn>
+          </div>
+        </v-window-item>
+        <v-window-item value="2">
+          <div class="bg-grey-lighten-5 pa-3" style="position: relative">
+            <prism class="bg-grey-lighten-5">
+              {{
+                ` 
+&lt;script setup&gt;
+${firstExampleScript}
+&lt;/script&gt;
+            `
+              }}
+            </prism>
+            <v-btn
+              class="copy-icon"
+              variant="text"
+              color="gray-400"
+              @click="copyToClipboard(firstExampleScript, 'secondScript')"
+            >
+              <v-icon
+                v-if="copiedValue == 'secondScript'"
+                size="x-large"
+                icon="mdi-clipboard-check-multiple-outline"
+                color="green-lighten-1"
+              />
+              <v-icon v-else size="x-large" icon="mdi-content-copy" />
+            </v-btn>
+          </div>
+        </v-window-item>
+      </v-window>
+    </div>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import 'prismjs';
+import 'prismjs/themes/prism.css';
+import Prism from 'vue-prism-component';
+const copiedValue = ref('');
+const copyToClipboard = async (message, item) => {
+  try {
+    await navigator.clipboard.writeText(message);
+    copiedValue.value = item;
+  } catch (err) {
+    copiedValue.value = item;
+  }
+};
+
+const firstExampleActivePage = ref('1');
+const secondExampleActivePage = ref('1');
+const firstExampleTabs = [
+  {
+    label: 'Template',
+    value: '1',
+  },
+  {
+    label: 'Script',
+    value: '2',
+  },
+];
+const firstExampleTemplate = ref(`
+<alex-custom-pagination v-model="activePage" :length="7" />
+  `);
+const firstExampleScript = ref(
+  `
+import { ref } from 'vue';
+const activePage = ref(1);
+`,
+);
+const secondExampleTemplate = ref(
+  `
+  <alex-custom-pagination v-model="activePage" :length="7" :total-visible="5" />
+    `,
+);
+</script>
+
+<style>
+.container {
+  border: 1px solid #a0a8b1;
+  gap: 16px;
+  align-self: stretch;
+}
+
+.align-self-stretch {
+  align-self: stretch;
+}
+
+.page {
+  max-width: 1200px;
+  gap: 40px;
+}
+
+.copy-icon {
+  position: absolute;
+  right: 24px;
+  top: 24px;
+  color: #a0a8b1;
+}
+</style>
