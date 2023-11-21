3b14d017ab0b8f45a200cd6c49579b361be584be
zignaggo <jvz1@aluno.ifal.edu.br>
2023-11-21 19:16:03 -0300

    feat: project card almost ready

---
+++ components/alex/learningplan/Card/Info.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex gap-2 align-center justify-start width">
+    <alex-custom-chip
+      size="x-large"
+      icon="mdi-account"
+      variant="outlined"
+      color="primary"
+    />
+    <div class="d-flex flex-column">
+      <h4 class="text-body-4 text-gray-800">{{ title }}</h4>
+      <p class="text-body-3 text-gray-600">{{ subtitle }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{ icon: string; title: string; subtitle: string | number }>(),
+  {},
+);
+</script>
+
+<style scoped>
+.width {
+  width: fit-content;
+}
+</style>
---
+++ components/alex/learningplan/Card/index.vue
@@ -0,0 +1,268 @@
+<template>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-card
+      v-bind="props"
+      :elevation="isHovering ? 4 : 0"
+      :min-width="width.min"
+      :max-width="width.max"
+      :class="isVertical ? 'vertical-grid' : 'horizontal-grid pa-2'"
+      variant="outlined"
+      color="gray-100"
+      rounded="lg"
+      class="grid bg-white"
+      @click="() => console.log('a')"
+    >
+      <div :class="{ rounded: !isVertical }" class="header">
+        <v-img
+          :src="image.url"
+          :alt="image.alt"
+          :class="{ grayscale: hide }"
+          cover
+          height="100%"
+          aspect-ratio="1.8"
+        />
+
+        <alex-custom-chip
+          :text="$t(`components.learningPlan.card.type.${type}`)"
+          size="small"
+          status="dark"
+          class="type"
+          variant="elevated"
+        />
+        <v-tooltip
+          :text="$t('components.learningPlan.card.hidden')"
+          location="bottom center"
+        >
+          <template v-slot:activator="{ props }">
+            <alex-custom-chip
+              v-if="hide"
+              v-bind="props"
+              class="hidden-icon"
+              size="x-large"
+              status="dark"
+              icon="mdi-eye-off-outline"
+              variant="elevated"
+            >
+            </alex-custom-chip>
+          </template>
+        </v-tooltip>
+
+        <div :class="{ hover: isHovering }">
+          <v-menu v-if="!hideFavorited">
+            <template #activator="{ props: propsMenu }">
+              <v-tooltip
+                :text="$t('components.learningPlan.card.favorite')"
+                location="bottom center"
+              >
+                <template #activator="{ props }">
+                  <v-btn-secondary
+                    v-if="isHovering"
+                    v-bind="{ ...propsMenu, ...props }"
+                    icon="mdi-cards-heart"
+                    class="favorite"
+                    :class="{ 'text-error-0': favorited }"
+                    @click="() => console.log('favoritar')"
+                  />
+                </template>
+              </v-tooltip>
+            </template>
+          </v-menu>
+          <v-menu
+            v-model="options"
+            :close-on-content-click="false"
+            :class="{ hidden: !isHovering && !options }"
+          >
+            <template #activator="{ props: propsMenu, isActive }">
+              <v-tooltip
+                :text="$t('components.learningPlan.card.options')"
+                location="bottom center"
+              >
+                <template #activator="{ props }">
+                  <v-btn-secondary
+                    v-if="
+                      direction !== 'HORIZONTAL' && (isHovering || isActive)
+                    "
+                    v-bind="{ ...propsMenu, ...props }"
+                    icon="mdi-dots-vertical"
+                    class="options"
+                  />
+                </template>
+              </v-tooltip>
+            </template>
+            <v-list>
+              <v-list-item
+                v-if="hide"
+                :title="$t('components.learningPlan.card.visibility.show')"
+                prepend-icon="mdi-eye-outline"
+                @click="() => console.log('mostrar')"
+              />
+              <v-list-item
+                v-else
+                :title="$t('components.learningPlan.card.visibility.hide')"
+                prepend-icon="mdi-eye-off-outline"
+                @click="() => console.log('Ocultar')"
+              />
+              <v-list-item
+                :title="$t('components.learningPlan.card.configurations')"
+                prepend-icon="mdi-cog-outline"
+                @click="() => console.log('Configurações')"
+              />
+            </v-list>
+          </v-menu>
+        </div>
+      </div>
+      <div
+        class="d-flex flex-column pa-4 gap-4"
+        :class="{ 'grayscale-2': hide }"
+      >
+        <div class="d-flex flex-column pa-0 gap-2">
+          <h5 class="text-h5 text-gray-900 ellipsis lines-2">{{ name }}</h5>
+          <v-card-subtitle
+            class="text-body-3 text-gray-600 ellipsis lines-3"
+            lines="three"
+            >{{ description }}</v-card-subtitle
+          >
+        </div>
+
+        <div class="d-flex gap-6 flex-wrap py-2">
+          <alex-learningplan-card-info
+            icon="mdi-person"
+            :title="$t('components.learningPlan.card.facilitator')"
+            :subtitle="facilitator"
+          />
+          <alex-learningplan-card-info
+            icon="mdi-trails"
+            :title="$t('components.learningPlan.card.trails')"
+            :subtitle="trailsNumber"
+          />
+        </div>
+      </div>
+
+      <div v-if="!isVertical" class="h-full">
+        <v-menu :close-on-content-click="false">
+          <template #activator="{ props }">
+            <v-btn-tertiary v-bind="props" icon="mdi-dots-vertical" />
+          </template>
+          <v-list>
+            <v-list-item
+              v-if="hide"
+              :title="$t('components.learningPlan.card.visibility.show')"
+              prepend-icon="mdi-eye-outline"
+              @click="() => console.log('mostrar')"
+            />
+            <v-list-item
+              v-else
+              :title="$t('components.learningPlan.card.visibility.hide')"
+              prepend-icon="mdi-eye-off-outline"
+              @click="() => console.log('Ocultar')"
+            />
+            <v-list-item
+              :title="$t('components.learningPlan.card.configurations')"
+              prepend-icon="mdi-cog-outline"
+              @click="() => console.log('Configurações')"
+            />
+          </v-list>
+        </v-menu>
+      </div>
+    </v-card>
+  </v-hover>
+</template>
+
+<script setup lang="ts">
+interface LearningPlanCard {
+  type: 'project' | 'course' | (string & {});
+  image: { url: string; alt?: string };
+  name: string;
+  description: string;
+  facilitator: string;
+  trailsNumber: number;
+  hide?: boolean;
+  hideFavorited?: boolean;
+  favorited?: boolean;
+  direction?: 'HORIZONTAL' | 'VERTICAL';
+}
+const props = withDefaults(defineProps<LearningPlanCard>(), {
+  hideFavorited: false,
+  favorited: false,
+  direction: 'VERTICAL',
+  hide: false,
+});
+const options = ref(false);
+const isVertical = computed(() => props.direction === 'VERTICAL');
+const width = computed(() =>
+  props.direction === 'VERTICAL'
+    ? { min: 300, max: 400 }
+    : { min: 688, max: 959 },
+);
+</script>
+
+<style scoped lang="scss">
+.grid {
+  display: grid;
+  align-content: stretch;
+}
+
+.vertical-grid {
+  grid-template-rows: 250px 1fr;
+  grid-template-columns: 1fr;
+}
+
+.horizontal-grid {
+  grid-template-columns: minmax(200px, 300px) minmax(400px, 1fr) auto;
+  grid-template-rows: 1fr;
+}
+
+.header {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.type,
+.favorite,
+.hidden-icon,
+.options,
+.hover {
+  position: absolute;
+}
+.type {
+  top: 16px;
+  left: 16px;
+}
+.hidden-icon {
+  bottom: 16px;
+  right: 16px;
+  z-index: 1;
+}
+
+.favorite {
+  bottom: 16px;
+  left: 16px;
+}
+.options {
+  top: 16px;
+  right: 16px;
+}
+
+.hover {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #0005;
+  transition: background 300ms ease-in-out;
+}
+
+.hidden {
+  visibility: hidden;
+}
+
+.grayscale {
+  filter: grayscale(100%);
+}
+.grayscale-2 {
+  filter: grayscale(100%);
+  opacity: 0.6;
+}
+</style>
