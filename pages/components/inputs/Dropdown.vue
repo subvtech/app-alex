02f13ab393bb11dd4d8bf2c594a69cb67c8f0b94
chrixtianny <christianny@live.com>
2023-11-25 13:28:19 -0300

    ajuste documentação

--- pages/components/inputs/Dropdown.vue
+++ pages/components/inputs/Dropdown.vue
@@ -285,7 +285,7 @@
     <p class="text-subtitle-2 text-gray-500">
       A props <strong>location</strong> define a localização do dropdown,
       podendo ser <strong>start</strong>, <strong>end</strong>,
-      <strong>top</strong> ou <strong>bottom</strong>.<br />
+      <strong>top</strong>, <strong>bottom</strong> ou <strong>center</strong>.<br />
       <Prism inline class="bg-white pa-0">{{
         `
         const options1 = ref({
