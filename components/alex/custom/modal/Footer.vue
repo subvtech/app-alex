fe5a002e79fd86b06f475ab83175276b7953e408
zignaggo <jvz1@aluno.ifal.edu.br>
2023-11-25 20:47:03 -0300

    feat: footer ok, header ok, modal ok, docs ok

---
+++ components/alex/custom/modal/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container
+    class="bg-white min-height-76 rounded-b-lg px-6 py-4 border-top-gray-100"
+  >
+    <v-row dense justify="end">
+      <v-col v-if="!noSecondButton" dense cols="auto">
+        <slot
+          v-if="hasSecondSlot"
+          name="secondButton"
+          :on-second-action="() => emits('onSecondAction')"
+        />
+        <v-btn
+          v-else
+          size="large"
+          color="gray-blue"
+          :text="nameSecondButton"
+          @click="() => emits('onSecondAction')"
+        />
+      </v-col>
+      <v-col v-if="!noMainButton" dense cols="auto">
+        <slot
+          v-if="hasMainSlot"
+          name="mainButton"
+          :on-main-action="() => emits('onMainAction')"
+        ></slot>
+        <v-btn
+          v-else
+          size="large"
+          :text="nameMainButton"
+          @click="() => emits('onMainAction')"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+interface HeaderProps {
+  noMainButton?: boolean;
+  noSecondButton?: boolean;
+  nameMainButton: string;
+  nameSecondButton: string;
+}
+withDefaults(defineProps<HeaderProps>(), {
+  noMainButton: false,
+  noSecondButton: false,
+  nameMainButton: 'Salvar',
+  nameSecondButton: 'Cancelar',
+});
+const emits = defineEmits(['onMainAction', 'onSecondAction']);
+const slots = useSlots();
+const hasMainSlot = computed(() => !!slots.mainButton);
+const hasSecondSlot = computed(() => !!slots.secondButton);
+</script>
+
+<style scoped>
+.border-top-gray-100 {
+  border-top: 1px solid rgb(var(--v-theme-gray-100)) !important;
+}
+.min-height-76 {
+  min-height: 72px;
+}
+</style>
---
+++ components/alex/custom/modal/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-dialog
+    max-width="720"
+    transition="dialog-center-transition"
+    :model-value="modelValue"
+    :activator="activator"
+    @update:model-value="handleChange"
+  >
+    <template v-if="hasCustomActivator" #activator="{ isActive, props }">
+      <slot name="activator" :is-active="isActive" :props="props" />
+    </template>
+    <slot
+      v-if="hasHeader"
+      name="header"
+      :title="title"
+      :highlight="highlight"
+      :on-close="() => emits('update:modelValue', false)"
+    />
+    <alex-custom-modal-header
+      v-else
+      :title="title"
+      :highlight="highlight"
+      @on-close="() => emits('update:modelValue', false)"
+    />
+    <v-container class="bg-white pa-6 gap-6 max-height">
+      <v-row dense>
+        <v-col dense>
+          <slot />
+        </v-col>
+      </v-row>
+    </v-container>
+    <slot
+      v-if="hasFooter"
+      name="footer"
+      :on-main-action="() => emits('onMainAction')"
+      :on-second-action="() => emits('onSecondAction')"
+    />
+    <alex-custom-modal-footer
+      v-else
+      :name-main-button="nameMainButton"
+      :name-second-button="nameSecondButton"
+      @on-main-action="() => emits('onMainAction')"
+      @on-second-action="() => emits('onSecondAction')"
+    />
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+interface HeaderProps {
+  modelValue: boolean;
+  activator?: 'parent';
+  title?: string;
+  highlight?: string;
+  nameMainButton?: string;
+  nameSecondButton?: string;
+}
+withDefaults(defineProps<HeaderProps>(), {
+  activator: undefined,
+  title: undefined,
+  highlight: undefined,
+  nameMainButton: undefined,
+  nameSecondButton: undefined,
+});
+const emits = defineEmits([
+  'update:modelValue',
+  'onMainAction',
+  'onSecondAction',
+]);
+const handleChange = (value) => {
+  emits('update:modelValue', value);
+};
+const slots = useSlots();
+const hasCustomActivator = computed(() => !!slots.activator);
+const hasHeader = computed(() => !!slots.header);
+const hasFooter = computed(() => !!slots.footer);
+</script>
+
+<style scoped>
+.max-height {
+  max-height: calc(100svh - 308px);
+  overflow-y: auto;
+}
+</style>
