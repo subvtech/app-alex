7a4cef5839427b778151f2a2e91826ff19198b9d
Joao Berta <joaovictorberta@gmail.com>
2023-11-30 13:35:15 +0000

    Update components.vue

--- layouts/components.vue
+++ layouts/components.vue
@@ -111,22 +111,6 @@ const menus = [
   },
 ];
 
-const profileMenuItems = [
-  {
-    title: i18n.t('layouts.default.profile'),
-    to: `/user/${user.value.username}`,
-    logout: false,
-  },
-  {
-    title: i18n.t('layouts.default.settings'),
-    to: '/user/settings',
-    logout: false,
-  },
-  {
-    title: i18n.t('layouts.default.logout'),
-    logout: true,
-  },
-];
 </script>
 
 <style scoped lang="scss">
