af6cfd46ce28ea0fe7d485381a41ad0bdd462bb7
zignaggo <jvz1@aluno.ifal.edu.br>
2023-12-05 11:17:55 -0300

    fix: add i18n, refact stepper field, refact name of emits

--- components/alex/inputs/stepper/Field.vue
+++ components/alex/inputs/stepper/Field.vue
@@ -1,47 +1,39 @@
 <template>
-  <alex-inputs-text-field
-    v-if="typeField === 'text'"
+  <component
+    :is="components[fieldType]"
     v-model="value"
     :error-messages="errorMessage"
-  >
-  </alex-inputs-text-field>
-  <v-autocomplete
-    v-else-if="typeField === 'autocomplete'"
-    v-model="value"
-    :error-messages="errorMessage"
-  >
-  </v-autocomplete>
-  <v-select
-    v-else-if="typeField === 'select'"
-    v-model="value"
-    :error-messages="errorMessage"
-  >
-  </v-select>
-  <v-textarea
-    v-else-if="typeField === 'textarea'"
-    v-model="value"
-    :error-messages="errorMessage"
-  >
-  </v-textarea>
+  />
 </template>
 
 <script setup lang="ts">
 import { useField } from 'vee-validate';
-const props = defineProps({
-  name: { type: String, required: true },
-  value: {
-    type: String || Number || Boolean || undefined,
-    default: undefined,
-  },
-  typeField: {
-    type: String as PropType<'autocomplete' | 'text' | 'textarea' | 'select'>,
-    default: 'text',
-  },
+import {
+  VAutocomplete,
+  VSelect,
+  VTextarea,
+} from 'vuetify/lib/components/index.mjs';
+import TextField from '@/components/alex/inputs/TextField.vue';
+const components = {
+  TextField,
+  VAutocomplete,
+  VTextarea,
+  VSelect,
+};
+type ComponentsKeys = keyof typeof components;
+
+interface FieldProps {
+  name: string;
+  value?: string | number | boolean;
+  fieldType?: ComponentsKeys;
+}
+
+const props = withDefaults(defineProps<FieldProps>(), {
+  value: undefined,
+  fieldType: 'TextField',
 });
 
 const { value, errorMessage } = useField(() => props.name, undefined, {
   initialValue: props.value,
 });
 </script>
-
-<style lang="scss" scoped></style>
