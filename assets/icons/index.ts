733d1dfb7ea8f046b85540c1ff96e744e1508079
Eliezir <eliezir2004@gmail.com>
2023-12-22 06:36:01 -0300

    refactor: deleting unnecessary code

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,12 +2,10 @@ import { h } from 'vue';
 import type { IconSet, IconProps } from 'vuetify';
 import trail from './Trail.vue';
 import DragIndicator from './DragIndicator.vue';
-import SortArrows from './SortArrows.vue';
 
 const alexAliases = {
   trail,
   DragIndicator,
-  SortArrows,
 };
 
 const alexIcons: IconSet = {
