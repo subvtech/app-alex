cf2812e4fee54e21b07e062e3890bce26ab0fc64
Joao Berta <joaovictorberta@gmail.com>
2023-12-22 21:00:27 +0000

    Revert "Merge branch 'Feature/104-ListarCursos' into 'develop'"
    This reverts merge request !155

--- composables/useVueI18n.ts
+++ composables/useVueI18n.ts
@@ -62,7 +62,6 @@ export async function loadLanguageAsync(lang: SUPPORT_LOCALES_TYPE) {
     'profile',
     'trailId',
     'forgot',
-    'classes',
     'emailConfirmation',
   ]);
   const components = await useImportLanguages('components', lang, [
