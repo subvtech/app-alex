647e258804f5f8c73aca1b798e245587668b6ad1
doubleyooz <sirjoandersonwf@gmail.com>
2023-12-25 19:20:34 -0300

    refactor: breakdown into smaller components

--- composables/useVueI18n.ts
+++ composables/useVueI18n.ts
@@ -65,7 +65,6 @@ export async function loadLanguageAsync(lang: SUPPORT_LOCALES_TYPE) {
     'forgot',
     'classes',
     'emailConfirmation',
-    'courseSettings',
   ]);
   const components = await useImportLanguages('components', lang, [
     'appLearningPlanCard',
