a19c2a5be18490ff0b585bf06500ea6c09ea01da
chrixtianny <christianny@live.com>
2024-01-20 17:47:50 -0300

    add trail Store, add load-trail middleware

---
+++ pages/courses/[id]/trails/[trailId].vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <alex-learningplan-trails-header
+      :trails-description="trailStore.trail.description"
+      :trails-cover="trailStore.trail.cover_image_url"
+      :trail-id="trailId"
+      :course-id="id"
+      :course-title="learningPlanStore.learningPlan?.title"
+      :trails-title="trailStore.trail.title"
+    />
+    <NuxtPage @update="getTrailData()" />
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['load-trail'],
+  hideLearningPlanBanner: true,
+});
+
+const route = useRoute();
+const { trailId, id } = route.params;
+
+const trailStore = useTrailStore();
+const learningPlanStore = useLearningPlanStore();
+
+const getTrailData = async () => {
+  await trailStore.loadTrailData(parseInt(trailId.toString()));
+};
+</script>
