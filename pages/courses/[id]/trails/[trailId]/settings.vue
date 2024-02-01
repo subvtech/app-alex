39e250b22ce08901a8d490a349a4bf055044052a
chrixtianny <christianny@live.com>
2024-02-01 01:36:59 -0300

    fix: use header store

--- pages/courses/[id]/trails/[trailId]/settings.vue
+++ pages/courses/[id]/trails/[trailId]/settings.vue
@@ -36,11 +36,12 @@ const { update } = useStrapi();
 const { setMessage } = useMessageStore();
 const { t } = useI18n();
 const route = useRoute();
-const { trailId } = route.params;
+const { trailId, id } = route.params;
 const trailStore = useTrailStore();
 const trailTitle = ref('');
 const trailDescription = ref('');
 const visibilityButton = ref('');
+const headerStore = usePageHeaderStore();
 
 const handleUpdateGeneral = (name, description) => {
   trailTitle.value = name;
@@ -67,6 +68,35 @@ const updateVisibility = async () => {
   });
   setMessage(t('components.trails.settings.visibilityUpdate'), 'green', true);
 };
+
+const learningPlanStore = useLearningPlanStore();
+
+onBeforeMount(() => {
+  headerStore.showHeader = true;
+  headerStore.title = t('components.trails.header.breadcrumbs.title');
+  headerStore.items = [
+    {
+      title: t('components.trails.header.breadcrumbs.1.title'),
+      disabled: false,
+      href: '/courses/me',
+    },
+    {
+      title: learningPlanStore.learningPlan.title,
+      disabled: false,
+      href: `/courses/${id}`,
+    },
+    {
+      title: trailStore.trail.title,
+      disabled: false,
+      href: `/courses/${id}/trails/${trailId}`,
+    },
+    {
+      title: t('components.trails.settings.title'),
+      disabled: false,
+      href: `/courses/${id}/trails/${trailId}/settings`,
+    },
+  ];
+});
 </script>
 <style scoped lang="scss">
 .w-201 {
