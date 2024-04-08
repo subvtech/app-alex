ffae9cd2032f83014e4450f314e1ef9e3950156d
Eliezir <eliezir2004@gmail.com>
2024-04-08 16:31:39 -0300

    fix: showing class name instead of identifier to students and formatting slug at courses config

--- stores/useLearningPlanStore.ts
+++ stores/useLearningPlanStore.ts
@@ -168,6 +168,12 @@ export const useLearningPlanStore = defineStore('learning-plan', () => {
     );
   });
 
+  const userClass = computed(() => {
+    return learningPlan.value?.classes.find(
+      (c) => c.learning_plan_members?.some((m) => m.user.id === user.value.id),
+    );
+  });
+
   const schedules = computed<LearningPlanScheduleSimple[]>(() => {
     return (
       learningPlan.value?.schedules.map((schedule) => {
@@ -201,6 +207,7 @@ export const useLearningPlanStore = defineStore('learning-plan', () => {
     userIsFacilitator,
     userIsActiveMember,
     userIsPendingMember,
+    userClass,
     activeInviteLinks,
     standardTrailsCount,
     standardTrails,
