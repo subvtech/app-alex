94094c7b747ad0b4e43b60ce1aad65fe21fb15e3
Eliezir <eliezir2004@gmail.com>
2024-07-03 12:50:39 -0300

    feat: testing default TipTap and @syfxlin/tiptap-starter-kit

---
+++ components/TipTap/index.vue
@@ -0,0 +1,304 @@
+<template>
+  <div class="container">
+    <div class="control-group">
+      <label>
+        <input type="checkbox" :checked="isEditable" @change="toggleEditable" />
+        Editable
+      </label>
+    </div>
+    <div v-if="editor" :editor="editor" :tippy-options="{ duration: 100 }">
+      <div class="floating-menu">
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+          :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+        >
+          H1
+        </button>
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+          :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+        >
+          H2
+        </button>
+        <button
+          @click="editor.chain().focus().toggleBulletList().run()"
+          :class="{ 'is-active': editor.isActive('bulletList') }"
+        >
+          Bullet list
+        </button>
+      </div>
+    </div>
+    <editor-content :editor="editor" />
+    {{ user?.username }}
+  </div>
+</template>
+
+<script setup>
+import { ref, watch, onMounted, onBeforeUnmount } from 'vue';
+import StarterKit from '@tiptap/starter-kit';
+import { Editor, EditorContent } from '@tiptap/vue-3';
+import Collaboration from '@tiptap/extension-collaboration';
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor';
+import { TiptapCollabProvider } from '@hocuspocus/provider';
+import * as Y from 'yjs';
+
+const doc = new Y.Doc();
+
+const editor = ref(null);
+const isEditable = ref(true);
+
+const collors = [
+  '#f783ac',
+  '#f3a683',
+  '#f3d683',
+  '#d9f683',
+  '#aef683',
+  '#83f6b4',
+  '#83f6f0',
+  '#83baf6',
+  '#838cf6',
+  '#c783f6',
+  '#f683e4',
+  '#f683a6',
+];
+
+onMounted(() => {
+  const user = useStrapiUser();
+  const provider = new TiptapCollabProvider({
+    name: encodeURIComponent('document.name'), // Unique document identifier for syncing. This is your document name.
+    appId: '7j9y6m10', // Your Cloud Dashboard AppID or `baseURL` for on-premises
+    token: 'notoken', // Your JWT token
+    document: doc,
+
+    // The onSynced callback ensures initial content is set only once using editor.setContent(), preventing repetitive content loading on editor syncs.
+    onSynced() {
+      if (!doc.getMap('config').get('initialContentLoaded') && editor) {
+        doc.getMap('config').set('initialContentLoaded', true);
+
+        editor.value.commands.setContent(`
+          <p>This is a radically reduced version of Tiptap. It has support for a document, with paragraphs and text. That’s it. It’s probably too much for real minimalists though.</p>
+          <p>The paragraph extension is not really required, but you need at least one node. Sure, that node can be something different.</p>
+          `);
+      }
+    },
+  });
+
+  editor.value = new Editor({
+    extensions: [
+      StarterKit,
+      Collaboration.configure({
+        document: doc,
+      }),
+      CollaborationCursor.configure({
+        provider,
+        user: {
+          name: user.value ? user.value.username : 'Anonymous',
+          color: collors[Math.floor(Math.random() * collors.length)],
+        },
+      }),
+    ],
+    content: props.modelValue,
+    onUpdate: ({ editor }) => {
+      // HTML
+      //   emits('update:modelValue', editor.getHTML());
+
+      // JSON
+      emits('update:modelValue', editor.getJSON());
+    },
+  });
+});
+
+const toggleEditable = () => {
+  isEditable.value = !isEditable.value;
+};
+
+watch(isEditable, (value) => {
+  if (editor.value) {
+    editor.value.setEditable(value);
+  }
+});
+
+onBeforeUnmount(() => {
+  if (editor.value) {
+    editor.value.destroy();
+  }
+});
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+
+const emits = defineEmits(['update:modelValue']);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    if (!editor.value) {
+      return;
+    }
+    // HTML
+    // const isSame = editor.value.getHTML() === value;
+
+    // JSON
+    const isSame =
+      JSON.stringify(editor.value.getJSON()) === JSON.stringify(value);
+
+    if (!isSame) {
+      editor.value.commands.setContent(value, false);
+    }
+  },
+);
+</script>
+
+<style lang="scss">
+/* Basic editor styles */
+.tiptap {
+  :first-child {
+    margin-top: 0;
+  }
+
+  /* List styles */
+  ul,
+  ol {
+    padding: 0 1rem;
+    margin: 1.25rem 1rem 1.25rem 0.4rem;
+
+    li p {
+      margin-top: 0.25em;
+      margin-bottom: 0.25em;
+    }
+  }
+
+  /* Heading styles */
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+    margin-top: 2.5rem;
+    text-wrap: pretty;
+  }
+
+  h1,
+  h2 {
+    margin-top: 3.5rem;
+    margin-bottom: 1.5rem;
+  }
+
+  h1 {
+    font-size: 1.4rem;
+  }
+
+  h2 {
+    font-size: 1.2rem;
+  }
+
+  h3 {
+    font-size: 1.1rem;
+  }
+
+  h4,
+  h5,
+  h6 {
+    font-size: 1rem;
+  }
+
+  /* Code and preformatted text styles */
+  code {
+    background-color: var(--purple-light);
+    border-radius: 0.4rem;
+    color: var(--black);
+    font-size: 0.85rem;
+    padding: 0.25em 0.3em;
+  }
+
+  pre {
+    background: var(--black);
+    border-radius: 0.5rem;
+    color: var(--white);
+    font-family: 'JetBrainsMono', monospace;
+    margin: 1.5rem 0;
+    padding: 0.75rem 1rem;
+
+    code {
+      background: none;
+      color: inherit;
+      font-size: 0.8rem;
+      padding: 0;
+    }
+  }
+
+  blockquote {
+    border-left: 3px solid rgb(var(--v-theme-gray-300));
+    margin: 1.5rem 0;
+    padding-left: 1rem;
+  }
+
+  hr {
+    border: none;
+    border-top: 1px solid rgb(var(--v-theme-gray-200));
+    margin: 2rem 0;
+  }
+  background-color: #fff;
+  padding: 18px;
+  border-radius: 8px;
+}
+
+.collaboration-cursor__caret {
+  border-left: 1px solid #0d0d0d;
+  border-right: 1px solid #0d0d0d;
+  margin-left: -1px;
+  margin-right: -1px;
+  pointer-events: none;
+  position: relative;
+  word-break: normal;
+}
+
+/* Render the username above the caret */
+.collaboration-cursor__label {
+  border-radius: 3px 3px 3px 0;
+  color: #0d0d0d;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  left: -1px;
+  line-height: normal;
+  padding: 0.1rem 0.3rem;
+  position: absolute;
+  top: -1.4em;
+  user-select: none;
+  white-space: nowrap;
+}
+
+/* Floating menu */
+.floating-menu {
+  display: flex;
+  background-color: rgb(var(--v-theme-gray-blue));
+  padding: 0.1rem;
+  border-radius: 0.5rem;
+
+  button {
+    background-color: unset;
+    padding: 0.275rem 0.425rem;
+    border-radius: 0.3rem;
+
+    &:hover {
+      background-color: rgb(var(--v-theme-gray-200));
+    }
+
+    &.is-active {
+      background-color: #fff;
+      color: var(--purple);
+
+      &:hover {
+        color: var(--purple-contrast);
+      }
+    }
+  }
+}
+</style>
---
+++ pages/TipTap.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex ga-5 flex-column">
+    <div class="pa-4">
+      <p class="text-primary text-h2">TipTap padrão</p>
+      <TipTap v-model="editorData" />
+      <code>{{ editorData }}</code>
+    </div>
+    <div class="pa-4">
+      <p class="text-primary text-h2">TipTap StarterKit</p>
+      <TipTap-Starter-kit v-model="editorData2" />
+      <code>{{ editorData2 }}</code>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const editorData = ref('');
+const editorData2 = ref('');
+</script>
