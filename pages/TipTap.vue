afa00d70af7cfbfef6a3ae89bbca43cc6fe15c38
Eliezir <eliezir2004@gmail.com>
2024-07-16 09:53:21 -0300

    refactor: removing syfxlin/tiptap-starter-kit

--- pages/TipTap.vue
+++ pages/TipTap.vue
@@ -5,17 +5,11 @@
       <TipTap v-model="editorData" />
       <p class="output">{{ editorData }}</p>
     </div>
-    <!-- <div class="pa-4">
-      <p class="text-primary text-h2">TipTap StarterKit</p>
-      <TipTap-Starter-kit v-model="editorData2" />
-      <p class="output">{{ editorData2 }}</p>
-    </div> -->
   </div>
 </template>
 
 <script setup>
 const editorData = ref('');
-const editorData2 = ref('');
 </script>
 
 <style scoped>
