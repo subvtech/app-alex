bda46b0647191548125130ea24a4fcc9d701af2b
zignaggo <jvz1@aluno.ifal.edu.br>
2024-08-15 20:50:42 -0300

    fix: z-index sidebar

--- components/alex/custom/sidebar/index.vue
+++ components/alex/custom/sidebar/index.vue
@@ -7,11 +7,11 @@
   <DialogRoot v-if="!isPermanent" v-model:open="drawer">
     <DialogPortal>
       <DialogOverlay
-        class="tw-fixed tw-inset-0 tw-z-[1006] tw-bg-black/30 data-[state=open]:tw-animate-in data-[state=closed]:tw-animate-out data-[state=closed]:tw-fade-out-0 data-[state=open]:tw-fade-in-0"
+        class="tw-fixed tw-inset-0 tw-z-[1200] tw-bg-black/30 data-[state=open]:tw-animate-in data-[state=closed]:tw-animate-out data-[state=closed]:tw-fade-out-0 data-[state=open]:tw-fade-in-0"
         side="left"
       />
       <DialogContent
-        class="tw-fixed tw-z-[1006] tw-gap-4 tw-shadow-lg tw-transition tw-ease-in-out data-[state=open]:tw-animate-in data-[state=closed]:tw-animate-out data-[state=closed]:tw-duration-200 data-[state=open]:tw-duration-200 tw-inset-y-0 tw-left-0 tw-h-full tw-w-[256px] data-[state=closed]:tw-slide-out-to-left data-[state=open]:tw-slide-in-from-left sm:tw-max-w-sm tw-flex"
+        class="tw-fixed tw-z-[1200] tw-gap-4 tw-shadow-lg tw-transition tw-ease-in-out data-[state=open]:tw-animate-in data-[state=closed]:tw-animate-out data-[state=closed]:tw-duration-200 data-[state=open]:tw-duration-200 tw-inset-y-0 tw-left-0 tw-h-full tw-w-[256px] data-[state=closed]:tw-slide-out-to-left data-[state=open]:tw-slide-in-from-left sm:tw-max-w-sm tw-flex"
       >
         <alex-custom-sidebar-bar :clipped="false" :blocks="blocks" />
       </DialogContent>
