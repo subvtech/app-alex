63b974b555e486242b5e2b3cea41af227641dec6
djalmajr <djalmajr@gmail.com>
2024-09-04 15:19:48 -0300

    updates

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,3 +1,172 @@
+<script setup lang="ts">
+import { TabType } from '~/components/alex/custom/Tabs.vue';
+
+definePageMeta({
+  middleware: ['auth'],
+  pageTransition: true,
+  validate: (route) => {
+    // Check if the id is made up of digits
+    return typeof route.params.id === 'string' && /^\D+$/.test(route.params.id)
+      ? {
+          statusCode: 404,
+          cause: 'invalid_params',
+          message: 'Invalid params',
+        }
+      : true;
+  },
+});
+
+const { t } = useI18n();
+const user = useStrapiUser<User>();
+const route = useRoute();
+const headerStore = usePageHeaderStore();
+const currentRouteTab = ref<number | null>(null);
+const learningPlanStore = useLearningPlanStore();
+
+const isJoinRoutePath = computed(() => {
+  if (!route?.name) return false;
+  return route.name === 'projects-id-join-hash';
+});
+
+const isSettingsRoutePath = computed(() => {
+  return route.name === 'projects-id-settings';
+});
+
+const generalLinks = computed<TabType[]>(() => [
+  {
+    label: t('pages.projects.overview'),
+    value: 0,
+    to: `/projects/${route.params.id}`,
+  },
+  {
+    label: t('pages.projects.documents'),
+    value: 1,
+    to: `/projects/${route.params.id}/documents`,
+  },
+  {
+    label: t('pages.projects.tasks'),
+    value: 2,
+    to: `/projects/${route.params.id}/tasks`,
+  },
+  {
+    label: t('pages.projects.members'),
+    value: 3,
+    to: `/projects/${route.params.id}/members`,
+  },
+  {
+    label: t('pages.projects.individual_learning'),
+    value: 4,
+    to: `/projects/${route.params.id}/individual_learning`,
+  },
+  ...(learningPlanStore.userIsFacilitator
+    ? [
+        {
+          label: '',
+          icon: 'mdi-cog-outline',
+          value: 5,
+          to: `/projects/${route.params.id}/settings`,
+          classes: 'ml-auto',
+        },
+      ]
+    : []),
+]);
+
+const fetchData = async () => {
+  headerStore.showHeader = true;
+  await learningPlanStore.loadLearningPlan(+route.params.id);
+  headerStore.isLoading = false;
+
+  if (!learningPlanStore.learningPlan) {
+    return navigateTo('/projects/me');
+  }
+
+  if (isSettingsRoutePath.value && !learningPlanStore.userIsFacilitator) {
+    return navigateTo(`/projects/${route.params.id}`);
+  }
+
+  if (
+    !learningPlanStore.userIsFacilitator &&
+    !learningPlanStore.userIsActiveMember &&
+    !learningPlanStore.userIsPendingMember &&
+    !isJoinRoutePath.value
+  ) {
+    return navigateTo('/projects/me');
+  }
+
+  if (learningPlanStore.userIsPendingMember && !isJoinRoutePath.value) {
+    const invite = learningPlanStore.learningPlan?.invitation_links.find(
+      (v) => v.emails_to_send?.includes(user?.value?.email),
+    );
+
+    if (invite) {
+      return navigateTo(`/projects/${route.params.id}/join/${invite.hash}`);
+    }
+  }
+};
+
+const selectRouteTab = (index: number | null) => {
+  currentRouteTab.value = index;
+};
+
+watch(
+  () => route.name,
+  () => {
+    const page = route.name?.toString() || '';
+    if (page.includes('projects-id')) fetchData();
+  },
+);
+
+watch(
+  () => learningPlanStore.loading,
+  () => {
+    if (learningPlanStore.loading) return;
+    const { id, memberId } = route.params;
+    const slug = route.name?.toString()?.split('-').at(-1);
+
+    headerStore.title = t('pages.projects.my_projects');
+    headerStore.items = [
+      {
+        title: t('pages.projects.my_projects'),
+        to: '/projects/me',
+      },
+      {
+        title: learningPlanStore.learningPlan?.title || '',
+        to: `/projects/${id}`,
+        disabled: !memberId,
+      },
+    ];
+
+    if (route.path.includes('/individual_learning')) {
+      headerStore.items.push({
+        title: t('pages.projects.individual_learning'),
+        to: `/projects/${id}/individual_learning`,
+        disabled: !memberId,
+      });
+
+      if (memberId) {
+        headerStore.title = t('pages.projects.individual_learning');
+        headerStore.items.push({
+          title: t(`pages.projects.${slug}`),
+          disabled: true,
+          to: `/projects/${id}/individual_learning/${memberId}/${slug}`,
+        });
+      }
+    }
+  },
+);
+
+onBeforeMount(async () => {
+  headerStore.isLoading = true;
+  await fetchData();
+});
+
+onBeforeUnmount(() => {
+  learningPlanStore.learningPlan = undefined;
+  learningPlanStore.loading = true;
+  headerStore.isLoading = true;
+});
+</script>
+
 <template>
   <section>
     <alex-custom-banner
@@ -19,8 +188,8 @@
         learningPlanStore.activeInvitationLinkUrl &&
         learningPlanStore.userIsFacilitator
           ? {
-              label: $t('pages.courses.invite'),
               copyText: learningPlanStore.activeInvitationLinkUrl,
+              label: $t('pages.courses.invite'),
             }
           : undefined
       "
@@ -28,7 +197,7 @@
         label: '',
         icon: 'mdi-cog-outline',
         value: 5,
-        to: `/projects/${learningPlanId}/settings`,
+        to: `/projects/${route.params.id}/settings`,
       }"
       show-profile-picture
       darker-background
@@ -40,162 +209,3 @@
     <NuxtPage @update="fetchData" />
   </section>
 </template>
-<script setup lang="ts">
-import { TabType } from '~/components/alex/custom/Tabs.vue';
-
-definePageMeta({
-  middleware: ['auth'],
-  pageTransition: true,
-  validate: (route) => {
-    // Check if the id is made up of digits
-    if (typeof route.params.id === 'string' && /^\D+$/.test(route.params.id)) {
-      return {
-        statusCode: 404,
-        cause: 'invalid_params',
-        message: 'Invalid params',
-      };
-    }
-    return true;
-  },
-});
-
-const { t } = useI18n();
-const user = useStrapiUser<User>();
-const route = useRoute();
-const { id } = route.params;
-const headerStore = usePageHeaderStore();
-const currentRouteTab = ref<number | null>(null);
-const learningPlanStore = useLearningPlanStore();
-const isJoinRoutePath = computed(() => {
-  if (!route?.name) {
-    return false;
-  }
-  const isRoute = route.name === 'projects-id-join-hash';
-
-  return isRoute;
-});
-
-// Computed
-const isSettingsRoutePath = computed(() => {
-  return route.name === 'projects-id-settings';
-});
-const learningPlanId = computed(() => parseInt(route.params?.id.toString()));
-const pageRoute = computed(() => route.name);
-
-const generalLinks = computed<TabType[]>(() => [
-  {
-    label: t('pages.projects.overview'),
-    value: 0,
-    to: `/projects/${learningPlanId.value}`,
-  },
-  {
-    label: t('pages.projects.documents'),
-    value: 1,
-    to: `/projects/${learningPlanId.value}/documents`,
-  },
-  {
-    label: t('pages.projects.tasks'),
-    value: 2,
-    to: `/projects/${learningPlanId.value}/tasks`,
-  },
-  {
-    label: t('pages.projects.members'),
-    value: 3,
-    to: `/projects/${learningPlanId.value}/members`,
-  },
-  {
-    label: t('pages.projects.individual_learning'),
-    value: 4,
-    to: `/projects/${learningPlanId.value}/individual_learning`,
-  },
-  ...(learningPlanStore.userIsFacilitator
-    ? [
-        {
-          label: '',
-          icon: 'mdi-cog-outline',
-          value: 5,
-          to: `/projects/${learningPlanId.value}/settings`,
-          classes: 'ml-auto',
-        },
-      ]
-    : []),
-]);
-
-// Functions
-const fetchData = async () => {
-  headerStore.showHeader = true;
-  await learningPlanStore.loadLearningPlan(learningPlanId.value);
-  headerStore.isLoading = false;
-  if (!learningPlanStore.learningPlan) {
-    return navigateTo('/projects/me');
-  }
-
-  if (isSettingsRoutePath.value && !learningPlanStore.userIsFacilitator) {
-    return navigateTo(`/projects/${learningPlanId.value}`);
-  }
-
-  if (
-    !learningPlanStore.userIsFacilitator &&
-    !learningPlanStore.userIsActiveMember &&
-    !learningPlanStore.userIsPendingMember &&
-    !isJoinRoutePath.value
-  ) {
-    return navigateTo('/projects/me');
-  }
-
-  if (learningPlanStore.userIsPendingMember && !isJoinRoutePath.value) {
-    const invite = learningPlanStore.learningPlan?.invitation_links.find(
-      (i) => i.emails_to_send?.includes(user?.value?.email),
-    );
-
-    if (invite) {
-      return navigateTo(
-        `/projects/${learningPlanId.value}/join/${invite.hash}`,
-      );
-    }
-  }
-};
-const selectRouteTab = (index: number | null) => {
-  currentRouteTab.value = index;
-};
-
-// LifeCycle
-onBeforeMount(async () => {
-  headerStore.isLoading = true;
-  await fetchData();
-});
-
-onBeforeUnmount(() => {
-  learningPlanStore.learningPlan = undefined;
-  learningPlanStore.loading = true;
-  headerStore.isLoading = true;
-});
-
-watch(pageRoute, async () => {
-  const page = pageRoute.value?.toString() || '';
-  if (page.includes('projects-id')) {
-    await fetchData();
-  }
-});
-
-watch(
-  () => learningPlanStore.loading,
-  () => {
-    if (!learningPlanStore.loading) {
-      headerStore.title = t('pages.projects.my_projects');
-      headerStore.items = [
-        {
-          title: t('pages.projects.my_projects'),
-          to: '/projects/me',
-          disabled: false,
-        },
-        {
-          title: learningPlanStore.learningPlan?.title || '',
-          to: `/projects/${id}`,
-          disabled: true,
-        },
-      ];
-    }
-  },
-);
-</script>
--- pages/projects/[id]/individual_learning/[memberId]/trails.vue
+++ pages/projects/[id]/individual_learning/[memberId]/trails.vue
@@ -1,7 +1,267 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { TrailSimple } from '@/models/simple/trailSimple.model';
+
+const { t } = useI18n();
+const { update } = useStrapi();
+const { findOne } = useStrapiUtils();
+const learningPlanStore = useLearningPlanStore();
+const route = useRoute();
+
+const page = ref(1);
+const search = ref('');
+const showAddTrailDialog = ref(false);
+
+const ITEMS_PER_PAGE = 12;
+
+const learningStructureId = computed(() => {
+  const structures = learningPlanStore.learningPlan?.learning_structures || [];
+  const structure = structures.find((v) => v.type === 'standard');
+  return structure?.id || 0;
+});
+
+const trails = computed<TrailSimple[]>(() => {
+  return (
+    learningPlanStore.standardTrails?.map((trail) => {
+      const { blocks = [] } = trail.structures.at(-1) || {};
+      return { ...trail, blocks };
+    }) || []
+  );
+});
+
+const getShowingMessage = (total: number) => {
+  const itemsPerPage = search.value === '' ? ITEMS_PER_PAGE : total;
+
+  const to =
+    page.value * itemsPerPage > trails.value.length
+      ? trails.value.length
+      : page.value * itemsPerPage;
+
+  return to === 0
+    ? t('pages.trails.noData')
+    : t('pages.trails.showingData', {
+        to,
+        from: (page.value - 1) * itemsPerPage + 1,
+        total: trails.value.length,
+      });
+};
+
+const toggleVisibility = (index: number, id: number) => {
+  const hidden = !trails.value[index].hidden;
+
+  try {
+    learningPlanStore.standardTrails[index].hidden = hidden;
+    update('trails', id, { hidden });
+  } catch (error) {
+    learningPlanStore.standardTrails[index].hidden = !hidden;
+  }
+};
+
+const navigate = (trailId: number, page?: string) => {
+  const { id, memberId } = route.params;
+  const slug = page === 'settings' ? '/settings' : '';
+  navigateTo(
+    `/projects/${id}/individual_learning/${memberId}/trails/${trailId}${slug}`,
+  );
+};
+
+const handleCreatedTrail = async (id: number) => {
+  const newTrail = await findOne('trails', id, {
+    populate: ['cover_image', 'structures.blocks'],
+  });
+  learningPlanStore.standardTrails.unshift(newTrail.data as TrailSimple);
+  showAddTrailDialog.value = false;
+};
+</script>
 
 <template>
-  <div>
-    <h1>Trails</h1>
+  <div
+    class="tw-flex tw-flex-1 tw-flex-col tw-bg-white tw-rounded-lg tw-p-6 wrapper"
+  >
+    <div
+      class="d-flex flex-wrap w-100 gap-4 gap-sm-1"
+      :class="!trails.length ? 'justify-end' : 'justify-space-between mb-6'"
+    >
+      <alex-inputs-text-field
+        v-show="trails.length"
+        v-model="search"
+        :placeholder="$t('pages.trails.searchPlaceholder')"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        name="search"
+        size="default"
+        class="w-50"
+        style="min-width: 160px; max-width: 320px"
+        density="comfortable"
+        hide-details
+      />
+      <alex-custom-button
+        v-if="learningPlanStore.userIsFacilitator"
+        :disabled="!learningStructureId"
+        prepend-icon="mdi-plus"
+        size="large"
+        @click="showAddTrailDialog = true"
+      >
+        {{ $t('pages.trails.newTrail') }}
+      </alex-custom-button>
+    </div>
+    <div
+      v-if="!trails.length"
+      class="tw-flex tw-flex-1"
+      :class="
+        learningPlanStore.loading
+          ? ''
+          : 'align-center justify-center flex-column'
+      "
+    >
+      <div v-if="learningPlanStore.loading">
+        <alex-custom-skeleton
+          color="gray-200"
+          class="width-80 height-10 mb-6"
+          rounded="lg"
+        />
+        <div class="tw-flex tw-gap-6">
+          <alex-learningplan-skeleton-trail-card
+            v-for="index in 3"
+            :key="index"
+          />
+        </div>
+      </div>
+      <div v-else class="tw-flex tw-items-center tw-justify-center tw-flex-col">
+        <img
+          class="emptyProjects-img"
+          src="/images/emptyTrails.svg"
+          :alt="$t('pages.trails.emptyStateText')"
+        />
+        <p class="text-h3 text-gray-400 mt-4">
+          {{ $t('pages.trails.emptyStateText') }}
+        </p>
+      </div>
+    </div>
+    <div v-else class="tw-flex tw-w-full tw-flex-col tw-h-full tw-flex-1">
+      <v-data-iterator
+        v-model:search="search"
+        v-model:page="page"
+        class="d-flex flex-column justify-space-between flex-start flex-grow-1 flex-shrink-1 position-relative"
+        :items="trails ?? []"
+        :items-per-page="12"
+        :filter-keys="['name', 'description', 'blocks']"
+        style="flex-basis: 0"
+      >
+        <template #default="{ items }">
+          <div class="card-container w-100">
+            <alex-learningplan-trails-card
+              v-for="(item, index) in items"
+              :key="item.raw.title + index"
+              :hide="item.raw.hidden"
+              :name="item.raw.title"
+              :can-edit="learningPlanStore.userIsFacilitator"
+              :description="item.raw.description"
+              :image="{ url: item.raw?.cover_image?.url }"
+              :blocks="item.raw.blocks ?? []"
+              class="flex-stretch"
+              @toggle-visibility="toggleVisibility(index, item.raw.id)"
+              @configurations="navigate(item.raw.id, 'settings')"
+              @open="navigate(item.raw.id)"
+              @copy="console.log(item.raw.id)"
+            />
+          </div>
+        </template>
+        <template #footer="{ pageCount, groupedItems }">
+          <div
+            class="d-flex w-100 justify-space-between align-center pa-6 pb-0 flex-column flex-sm-row ga-3 footer mt-6"
+          >
+            <p class="show-cardlist text-body-3 text-gray-600">
+              {{ getShowingMessage(groupedItems.length) }}
+            </p>
+            <alex-custom-pagination
+              v-if="pageCount > 1"
+              v-model="page"
+              :length="pageCount"
+              :total-visible="5"
+              class="extra-mb"
+            />
+          </div>
+        </template>
+      </v-data-iterator>
+    </div>
+
+    <alex-learningplan-trails-dialogs-create
+      v-if="learningPlanStore.userIsFacilitator && !!learningStructureId"
+      :model-value="showAddTrailDialog"
+      :learning-structure="learningStructureId"
+      @course-created="handleCreatedTrail"
+      @update:model-value="(e) => (showAddTrailDialog = e)"
+    />
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  min-height: 500px !important;
+}
+.emptyProjects-img {
+  max-width: 400px;
+  max-height: 360px;
+}
+
+.flex-stretch {
+  box-sizing: border-box !important;
+}
+
+.card-container {
+  display: grid !important;
+  row-gap: 24px;
+  column-gap: 24px;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)) !important;
+  justify-content: center; /* Centers the grid items horizontally */
+  align-items: center;
+
+  display: grid !important;
+}
+
+.footer {
+  border-top: 1px #ebedef solid;
+  max-height: 95px;
+  align-self: flex-end !important;
+}
+
+.hidden {
+  opacity: 0.5;
+}
+
+@media (min-width: 959px) and (max-width: 976px) {
+  .card-container {
+    justify-content: center !important;
+    align-items: center !important;
+  }
+
+  .flex-stretch {
+    justify-self: center;
+  }
+}
+
+@media (max-width: 720px) {
+  .card-container {
+    justify-content: center !important;
+    align-items: center !important;
+  }
+
+  .flex-stretch {
+    justify-self: center;
+  }
+}
+
+@media (max-width: 477px) {
+  .show-cardlist {
+    font-size: 12px !important;
+    letter-spacing: 0.4px !important;
+  }
+}
+
+@media (max-width: 431px) {
+  .extra-mb {
+    translate: 0 -16px;
+  }
+}
+</style>
