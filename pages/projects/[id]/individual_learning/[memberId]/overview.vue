bd435050465ff59708a1d2ff6aecdf34a8ec97be
Eliezir <eliezir2004@gmail.com>
2024-09-07 19:28:44 -0300

    feat: card totalizers in members overview

--- pages/projects/[id]/individual_learning/[memberId]/overview.vue
+++ pages/projects/[id]/individual_learning/[memberId]/overview.vue
@@ -1,36 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardTotalizer from '../../-components/CardTotalizer.vue';
+
+const { t } = useI18n();
+
+// Todo - Add custom icons & i18n
+const totalizers = ref({
+  sprints: {
+    title: 'Total de tarefas',
+    icon: 'alex:Sprint',
+    value: '02',
+    percentage: 50,
+  },
+  epics: {
+    title: 'Total de objetivos',
+    icon: 'alex:ManageHistory',
+    value: '03',
+    percentage: 33,
+  },
+  stories: {
+    title: 'Total de contribuições',
+    icon: 'alex:HistoryEdu',
+    value: '21',
+    percentage: 10,
+    color: 'warning-0',
+    chipColor: 'warning--2',
+  },
+});
+</script>
 
 <template>
   <div>
-    <v-row>
-      <v-col cols="4">
-        <v-card>
-          <v-card-text>
-            <h3>Total de tarefas</h3>
-            <h2 class="text-h2">02</h2>
-            <p>50% Finalizadas</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-text>
-            <h3>Total de objetivos</h3>
-            <h2 class="text-h2">03</h2>
-            <p>33% Finalizados</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-text>
-            <h3>Total de contribuições</h3>
-            <h2 class="text-h2">21</h2>
-            <p>10% Destacados</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <div class="tw-flex tw-flex-wrap gap-4 tw-mb-5 tw-flex-wrap">
+      <CardTotalizer v-for="totalizer in Object.values(totalizers)" :key="totalizer.title" :totalizer="totalizer" />
+    </div>
     <v-row>
       <v-col cols="8">
         <v-card>
@@ -56,12 +58,8 @@
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>OA1</v-list-item-title>
-              <v-list-item-subtitle
-                >Consertar o Header e Sidebar do ALEX</v-list-item-subtitle
-              >
-              <v-list-item-subtitle
-                >Término: 2 de janeiro de 2024</v-list-item-subtitle
-              >
+              <v-list-item-subtitle>Consertar o Header e Sidebar do ALEX</v-list-item-subtitle>
+              <v-list-item-subtitle>Término: 2 de janeiro de 2024</v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-action>
               <v-chip color="green">Concluído</v-chip>
