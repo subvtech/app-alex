ae9749d56c1d089946332b52ba905256eeab56bf
Eliezir <eliezir2004@gmail.com>
2024-10-17 08:16:14 -0300

    feat: adding dropdown search option

--- components/alex/custom/Dropdown.vue
+++ components/alex/custom/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-menu :dark="isDarkMode" role="dropdown">
+  <v-menu :dark="isDarkMode" role="dropdown" @update:model-value="onDropdownVisibilityChange">
     <template #activator="{ props: propsActivator, isActive }">
       <alex-custom-button
         v-if="!hasActivatorSlot"
@@ -13,11 +13,24 @@
 
     <v-list
       data-testid="dropdown-content"
-      class="pa-0 py-2 rounded-lg my-2"
-      :class="{ 'bg-primary-2': isDarkMode, 'bg-white': !isDarkMode }"
+      class="pa-0 pb-2 rounded-lg my-2 position-relative minimalist-scrollbar"
+      :class="{ 'bg-primary-2': isDarkMode, 'bg-white': !isDarkMode, 'pt-2': !props.showSearch }"
     >
+      <div
+        v-if="props.showSearch"
+        class="px-4 py-2 tw-w-[230px] tw-sticky tw-top-0 tw-left-0 tw-right-0 bg-white"
+        style="z-index: 1000"
+        @click.stop
+      >
+        <alex-inputs-text-field
+          v-model="search"
+          :placeholder="props.searchPlaceholder"
+          density="compact"
+          hide-details
+        />
+      </div>
       <alex-custom-list-item
-        v-for="({ text, icon, link, warning, onClick, notBold }, index) in items"
+        v-for="({ text, icon, link, warning, onClick, notBold }, index) in filteredItems"
         :key="index"
         :text="text"
         :icon="icon"
@@ -47,6 +60,8 @@ interface DropdownProps {
   icon?: string;
   notBold?: boolean;
   onItemSelect?: (index: number) => unknown;
+  showSearch?: boolean;
+  searchPlaceholder?: string;
 }
 
 const props = withDefaults(defineProps<DropdownProps>(), {
@@ -57,11 +72,23 @@ const props = withDefaults(defineProps<DropdownProps>(), {
   icon: 'mdi-dots-vertical',
   notBold: false,
   onItemSelect: () => {},
+  showSearch: false,
+  searchPlaceholder: 'Search',
 });
 
+const search = ref('');
+const filteredItems = computed(() => {
+  return props.items.filter((item) => item.text.toLowerCase().includes(search.value.toLowerCase()));
+});
 const slots = useSlots();
 const isDarkMode = computed(() => props.theme === 'dark');
 const hasActivatorSlot = computed(() => !!slots.activator);
+
+const onDropdownVisibilityChange = (isVisible: boolean) => {
+  if (isVisible) {
+    search.value = '';
+  }
+};
 </script>
 
 <style scoped lang="scss">
